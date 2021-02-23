0e7bf30adf4352b15a3b4ec064815aa9feab236e
Nivq <30695715+Nivq@users.noreply.github.com>
2021-02-23 11:00:56 +0100

    WarningViewModel - Added some fields + methods

--- src/temperatureSimulatorGUI/viewmodel/WarningViewModel.java
+++ src/temperatureSimulatorGUI/viewmodel/WarningViewModel.java
@@ -1,5 +1,38 @@
 package temperatureSimulatorGUI.viewmodel;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import mediators.TemperatureModel;
+
 // todo: everything
 public class WarningViewModel
 {
+    DoubleProperty highWarning;
+    DoubleProperty lowWarning;
+
+    TemperatureModel temperatureModel;
+
+    public WarningViewModel(TemperatureModel temperatureModel)
+    {
+        this.temperatureModel = temperatureModel;
+
+        highWarning =  new SimpleDoubleProperty(40);
+        lowWarning =  new SimpleDoubleProperty(5);
+    }
+
+    public void spoilerAlert()
+    {
+        //if(highWarning.getValue() != null )
+        //TODO: mangler
+    }
+
+    public DoubleProperty highWarningProperty()
+    {
+        return highWarning;
+    }
+
+    public DoubleProperty lowWarningProperty()
+    {
+        return lowWarning;
+    }
 }
