229eaa201e1b99226e4c5ac414dd2ff0199108ce
SanderKirchert <Sanders@post.cybercity.dk>
2021-02-23 10:27:18 +0100

    add some to model some to core

--- src/mediators/TemperatureModelManager.java
+++ src/mediators/TemperatureModelManager.java
@@ -1,5 +1,22 @@
 package mediators;
 // todo: everything
-public class TemperatureModelManager
+
+import model.temperature.Temperature;
+
+public class TemperatureModelManager extends TemperatureModel
 {
+    @Override
+    public void addTemperature(String id, double temperature) {
+
+    }
+
+    @Override
+    public Temperature getLastInsertedTemperature() {
+        return null;
+    }
+
+    @Override
+    public Temperature getLastInsertedTemperature(String id) {
+        return null;
+    }
 }
